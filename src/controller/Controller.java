46ea596223084a7cac8891dffe04f100e27f84bd
chriz-d <chris.dorn@freenet.de>
2021-01-02 12:27:25 +0100

    fixed funky camera rotation

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -11,8 +11,11 @@ import com.jme3.input.MouseInput;
 import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.CameraNode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.system.AppSettings;
 import com.jme3.system.JmeCanvasContext;
@@ -37,7 +40,9 @@ public class Controller extends SimpleApplication {
 	/** GUI */
 	private View view;
 	/** Mesh for jMonkey to display */
-	private CSGShape csg;
+	private CSGShape currentDisplayedObject;
+	private CSGShape lastDisplayedObject;
+	
 	/** Time passed since last update for jMonkey */
 	private float updateTime = 0;
 	
@@ -85,7 +90,7 @@ public class Controller extends SimpleApplication {
 		treeMap.remove(blockToDelete);
 		shapeMap.remove(blockToDelete);
 		rootNode.detachAllChildren();
-		csg = null;
+		currentDisplayedObject = null;
 	}
 	
 	/**
@@ -179,7 +184,7 @@ public class Controller extends SimpleApplication {
 	/** Sets the mesh via lookup in map. Gets picked up by jMonkey thread. */
 	public void setDisplayedMesh(BlockComponent block) {
 		Shape shape = shapeMap.get(block);
-		csg = shape.generateCSGMesh();
+		currentDisplayedObject = shape.generateCSGMesh();
 	}
 	
 	public void addShape(BlockComponent block) {
@@ -208,7 +213,14 @@ public class Controller extends SimpleApplication {
 	public void simpleInitApp() {
 		flyCam.setEnabled(false);
 		flyCam.setDragToRotate(true);
-		enableCameraControls();
+		CameraNode camNode = new CameraNode("Camera", cam);
+		camNode.setLocalTranslation(0, 0, -10);
+		Node node = new Node("pivot");
+		node.attachChild(camNode);
+		camNode.getCamera().lookAt(new Vector3f(0, 0, 0), new Vector3f(0, 1, 0));
+		rootNode.attachChild(node);
+		enableCameraControls(node);
+		System.out.println(cam.getLocation());
 	}
 	
 	@Override
@@ -216,9 +228,12 @@ public class Controller extends SimpleApplication {
 		updateTime += tpf;
 		if(updateTime > 0.2) {
 			updateTime = 0;
-			if(csg != null && !rootNode.hasChild(csg)) {
-				rootNode.detachAllChildren();
-				rootNode.attachChild(csg);
+			if(currentDisplayedObject != null && !rootNode.hasChild(currentDisplayedObject)) {
+				if(lastDisplayedObject != null) {
+					rootNode.detachChild(lastDisplayedObject);
+				}
+				rootNode.attachChild(currentDisplayedObject);
+				lastDisplayedObject = currentDisplayedObject;
 			}
 		}
 		super.simpleUpdate(tpf);
@@ -227,13 +242,13 @@ public class Controller extends SimpleApplication {
 	/**
 	 * Adds camera controls to JMonkeys input manager (Rotation around center 0,0,0)
 	 */
-	private void enableCameraControls() {
+	private void enableCameraControls(Node node) {
 		inputManager.addMapping("Rotate Left", new MouseAxisTrigger(MouseInput.AXIS_X, true));
 		inputManager.addMapping("Rotate Right", new MouseAxisTrigger(MouseInput.AXIS_X, false));
 		inputManager.addMapping("Rotate Up", new MouseAxisTrigger(MouseInput.AXIS_Y, true));
 		inputManager.addMapping("Rotate Down", new MouseAxisTrigger(MouseInput.AXIS_Y, false));
 		inputManager.addMapping("Click", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-		JMEKeyListener listener = new JMEKeyListener(cam);
+		JMEKeyListener listener = new JMEKeyListener(node);
 		inputManager.addListener(listener, new String[] {"Rotate Left", "Rotate Right", "Rotate Up", "Rotate Down"});
 		inputManager.addListener(listener, new String[] {"Click"});
 	}
--- src/controller/JMEKeyListener.java
+++ src/controller/JMEKeyListener.java
@@ -6,14 +6,15 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
 
 public class JMEKeyListener implements AnalogListener, ActionListener {
 
 	private boolean mouseButtonPressed;
-	private Camera cam;
+	private Node node;
 	
-	public JMEKeyListener(Camera cam) {
-		this.cam = cam;
+	public JMEKeyListener(Node node) {
+		this.node = node;
 	}
 	
 	@Override
@@ -27,36 +28,22 @@ public class JMEKeyListener implements AnalogListener, ActionListener {
 
 	@Override
 	public void onAnalog(String name, float keyPressed, float tpf) {
+		Quaternion newRotation = null;
 		if(name.equals("Rotate Left") && mouseButtonPressed) {
-			Quaternion leftRotation = new Quaternion();
-			leftRotation.fromAngleAxis(FastMath.HALF_PI/80, Vector3f.UNIT_Y);
-			Vector3f rotationResult = leftRotation.mult(cam.getLocation());
-			cam.setLocation(rotationResult);
-			cam.lookAt(new Vector3f(0,0,0), new Vector3f(0,1,0));
+			newRotation = new Quaternion();
+			newRotation.fromAngleAxis(FastMath.HALF_PI/80, Vector3f.UNIT_Y);
 		} else if(name.equals("Rotate Right") && mouseButtonPressed) {
-			Quaternion rightRotation = new Quaternion();
-			rightRotation.fromAngleAxis(-FastMath.HALF_PI/80, Vector3f.UNIT_Y);
-			Vector3f rotationResult = rightRotation.mult(cam.getLocation());
-			cam.setLocation(rotationResult);
-			cam.lookAt(new Vector3f(0,0,0), new Vector3f(0,1,0));
+			newRotation = new Quaternion();
+			newRotation.fromAngleAxis(-FastMath.HALF_PI/80, Vector3f.UNIT_Y);
 		} else if(name.equals("Rotate Up") && mouseButtonPressed) {
-			Quaternion upRotation = new Quaternion();
-			Quaternion upRotation2 = new Quaternion();
-			upRotation.fromAngleAxis(-FastMath.HALF_PI/80, Vector3f.UNIT_X);
-			upRotation2.fromAngleAxis(-FastMath.HALF_PI/80, Vector3f.UNIT_Z);
-			Vector3f rotationResult = upRotation.mult(cam.getLocation());
-			rotationResult = upRotation2.mult(rotationResult);
-			cam.setLocation(rotationResult);
-			cam.lookAt(new Vector3f(0,0,0), new Vector3f(0,1,0));
+			newRotation = new Quaternion();
+			newRotation.fromAngleAxis(FastMath.HALF_PI/80, Vector3f.UNIT_X);
 		} else if(name.equals("Rotate Down") && mouseButtonPressed) {
-			Quaternion downRotation = new Quaternion();
-			Quaternion downRotation2 = new Quaternion();
-			downRotation.fromAngleAxis(FastMath.HALF_PI/80, Vector3f.UNIT_X);
-			downRotation2.fromAngleAxis(FastMath.HALF_PI/80, Vector3f.UNIT_Z);
-			Vector3f rotationResult = downRotation.mult(cam.getLocation());
-			rotationResult = downRotation2.mult(rotationResult);
-			cam.setLocation(rotationResult);
-			cam.lookAt(new Vector3f(0,0,0), new Vector3f(0,1,0));
+			newRotation = new Quaternion();
+			newRotation.fromAngleAxis(-FastMath.HALF_PI/80, Vector3f.UNIT_X);
+		}
+		if(newRotation != null) {
+			node.setLocalRotation(node.getLocalRotation().mult(newRotation));
 		}
 	}
 
